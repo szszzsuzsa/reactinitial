fa3b4d89e90864a99e5d94a92bed28a885125712
szszzsuzsa <szokeszabo.zsuzsanna@gmail.com>
2021-04-09 14:02:00 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import './App.css'
+import './App.css';
+import LoadingMask from "./components/LoadingMask";
+import Laptop from "./components/Laptop";
+import {useState, useEffect} from "react";
 
 const App = () => {
 
+
+  const [laptops, setLaptops] = useState(null)
+  const [laptop, setLaptop] = useState(null)
+  const [response, setResponse] = useState(null)
+  const [filteredLaptop, setFilteredLaptop] = useState(null);
+
+  const url= "/api/laptop"
+
+  const getLaptop = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setLaptops(data);
+      
+      console.log(laptops);
+    } catch (error) {
+      setResponse(false);
+      //console.log(response)
+    }
+  };
+
+useEffect(() => {
+  getLaptop()
+}, [])
+
+
+
   return (
     <div className="App">
-      Todo...
+      <h1>Laptops</h1>
+
+
+      {laptops == null ? <LoadingMask /> : laptops.map((laptop, index) => <Laptop key={index} laptop={laptop.name}
+      brand={laptop.brand} weight={laptop.weight} />)}
+
     </div>
   )
 }
---
+++ src/components/Laptop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Laptop(laptop, key) {
+    return (
+        <div>
+            {laptop}
+
+            <button>Show more :</button>
+        </div>
+    )
+}
+
+export default Laptop
